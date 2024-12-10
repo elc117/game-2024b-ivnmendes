62cd47cfae24af4aa94bfa5c8c2c7df6e39977ec
Ivan <ivanmendes2005@gmail.com>
2024-12-09 22:16:51 -0300

    Feat: leitura de arquivo finalmente funcionando na web

--- core/src/main/java/com/paradigmas/game/utils/QuizGenerator.java
+++ core/src/main/java/com/paradigmas/game/utils/QuizGenerator.java
@@ -2,21 +2,41 @@ package com.paradigmas.game.utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
-import com.google.gson.Gson;
+import com.badlogic.gdx.utils.Json;
+import com.badlogic.gdx.utils.JsonReader;
+import com.badlogic.gdx.utils.JsonValue;
+import com.paradigmas.game.questions.Question;
 import com.paradigmas.game.questions.Quiz;
 
 public abstract class QuizGenerator {
     public static Quiz quizGenerate(int option) {
-        Gson gson = new Gson();
-
         String path = "questions/quiz-" + option + ".json";
+        Json json = new Json();
 
         FileHandle fileHandle = Gdx.files.internal(path);
         if (!fileHandle.exists()) {
             throw new RuntimeException("Arquivo não encontrado: " + path);
         }
 
-        String json = fileHandle.readString();
-        return gson.fromJson(json, Quiz.class);
+        String jsonString = fileHandle.readString();
+
+        JsonReader jsonReader = new JsonReader();
+        JsonValue jsonValue = jsonReader.parse(jsonString);
+        Quiz quiz = new Quiz();
+
+        JsonValue questions = jsonValue.get("questions");
+        for (JsonValue question : questions) {
+            String questionText = question.getString("question");
+            JsonValue optionsJson = question.get("options");
+            String[] options = new String[optionsJson.size];
+            for (int i = 0; i < optionsJson.size; i++) {
+                options[i] = optionsJson.getString(i);
+            }
+            String correctAnswer = question.getString("correctAnswer");
+
+            quiz.addQuestion(new Question(questionText, options, Integer.parseInt(correctAnswer)));
+        }
+
+        return quiz;
     }
 }
